12b9b899cd3091769e7a96954357b36abfcb6396
J Wynia <j@wynia.org>
2013-05-13 09:36:06 -0500

    Cleaned out project that had namespace issues.

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -4,7 +4,6 @@ using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ImageGenerator;
 
 namespace Sandbox
 {
@@ -12,12 +11,12 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            var generator = new ImageGenerator.ImageGenerator();
+            string xamlString = System.IO.File.ReadAllText("SampleTemplates/BlueButton.xaml");
+            string outputFilePath = "BlueButton.png";
+
             dynamic viewModel = new ExpandoObject();
-            string xamlString = "";
-            byte[] image = generator.GenerateImage(xamlString, viewModel);
-            string outputFilePath = "";
-            generator.GenerateImageFile(outputFilePath, xamlString, viewModel);
+            viewModel.Text = "Add to Cart";
+            ImageGenerator.CreateTextImage("BlueButton","BlueButton.png","Add to Cart");
         }
     }
 }
